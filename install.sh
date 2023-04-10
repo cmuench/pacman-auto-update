dfec5214dcae63e5203c7d7f2e98fbc61af1e571
Alberto Salvia Novella <es20490446e@gmail.com>
2023-04-10 02:32:13 +0200

    Fully automate install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,39 @@
 #! /bin/bash
 set -e
 
-git clone --depth 1 --shallow-submodules https://github.com/cmuench/pacman-auto-update.git
-cd pacman-auto-update/package
 
-makepkg --syncdeps --needed --rmdeps --force --noconfirm
-sudo pacman --upgrade *.pkg.*
+mainFunction () {
+	so rm --recursive --force pacman-auto-update
 
-cd ../../
-rm --recursive pacman-auto-update
+	so git clone --depth 1 --shallow-submodules "https://github.com/cmuench/pacman-auto-update.git"
+	cd "./pacman-auto-update/package"
+
+	export PACKAGER="${USER} <@${HOSTNAME}>"
+	so makepkg --syncdeps --needed --rmdeps --force --noconfirm
+	so sudo pacman --upgrade --noconfirm *.pkg.*
+
+	cd "../.."
+	so rm --recursive --force pacman-auto-update
+}
+
+
+so () {
+	local commands="${@}"
+
+	if [[ "${verbose}" -eq 1 ]]; then
+		if ! ${commands}; then
+			exit "${?}"
+		fi
+	elif ! error="$(eval "${commands}" 2>&1 >"/dev/null")" ; then
+		if [ "${error}" == "" ] ; then
+			error="Command failed: ${commands}"
+		fi
+
+		echo "${FUNCNAME[1]}: ${error}" >&2
+		exit 1
+	fi
+}
+
+
+sudo -v
+mainFunction
